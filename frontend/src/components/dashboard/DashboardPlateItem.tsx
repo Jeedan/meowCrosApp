0ddfbbc068c40c5c3b8060f10d755a5fa8443cbf
Jeedan <Jeedan.Cuneyt@gmail.com>
2026-05-29 20:42:05 +0300

    removed unused imports

--- frontend/src/components/dashboard/DashboardPlateItem.tsx
+++ frontend/src/components/dashboard/DashboardPlateItem.tsx
@@ -17,11 +17,6 @@ export default function DashboardPlateItem({ meal }: DashboardPlateItemProps) {
 }
 
 const styles = StyleSheet.create({
-	container: {
-		justifyContent: "center",
-		alignItems: "flex-start",
-	},
-
 	mealsContainer: {
 		borderColor: colors.textSecondary,
 		backgroundColor: colors.cardBackground,
@@ -29,16 +24,6 @@ const styles = StyleSheet.create({
 		borderWidth: 0,
 		paddingVertical: 6,
 		paddingHorizontal: 12,
-	},
-
-	shadow: {
-		shadowOffset: {
-			width: 0,
-			height: 2,
-		},
-		shadowOpacity: 0.55,
-		shadowRadius: 15,
-		elevation: 3,
 	},
 
 	mealsHeader: {
@@ -53,26 +38,5 @@ const styles = StyleSheet.create({
 		fontWeight: "600",
 		color: colors.textSecondary,
 		marginBottom: 4,
-	},
-
-	calorieText: {
-		color: colors.text,
-	},
-
-	// incase we want to expand the swipe menu
-	deleteContainer: {
-		flexDirection: "row",
-		justifyContent: "center",
-		alignItems: "center",
-		paddingLeft: 12,
-		gap: 8,
-	},
-
-	deleteButton: {
-		backgroundColor: colors.alert,
-		marginTop: 0,
-		paddingVertical: 12,
-		paddingHorizontal: 12,
-		borderRadius: 14,
 	},
 });
